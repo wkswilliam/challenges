098149e06c539f3d56928db260ae1fcbb8b84420
William Scariot <wkswilliam@gmail.com>
2020-07-27 18:32:45 -0300

    ttt game finished

--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -11,36 +11,87 @@ import random
 from itertools import combinations
 
 
-
-def play():
-    board = [['','',''],
-             ['','',''],
-             ['','','']]
-    available = [(0,0),(0,1),(0,2),
-                 (1,0),(1,1),(1,2),
-                 (2,0),(2,1),(2,2)]
-    random.choices(available) 
-
-def winner(arr):
-    arr = np.array(arr)
-    return (arr=='X').all() or (arr=='O').all()
+class TicTacToe():
     
-
-def check_winner(board):
-    # Line and Column
-    for i in range(3):
-        line = board[i]
-        column = []
-        for j in range(3):
-            column.append(board[j][i])
-        if winner(line) or winner(column):
+    def __init__(self, player=False):
+        self.player = player
+        self.board = [[None,None,None],
+                     [None,None,None],
+                     [None,None,None]]
+        self.available = [(0,0),(0,1),(0,2),
+                         (1,0),(1,1),(1,2),
+                         (2,0),(2,1),(2,2)]
+    
+    def check_valid_player_input(self, p):
+        try:
+            m = tuple(map(int, p.split(',')))
+        except:
+            print("Please Provide a valid input.")
             return True
-    # Diagonals
-    d_1 = [board[0][0],board[1][1],board[2][2]]
-    d_2 = [board[0][2],board[1][1],board[2][0]]
+        if m in self.available:
+            self.board[m[0]][m[1]] = True
+            self.available.remove(m)
+            return False
+        else:
+            return True
     
-    return winner(d_1) or winner(d_2)
-
+    def check_winner(self):
+        def winner(arr):
+            arr = np.array(arr)
+            return (arr==False).all() or (arr==True).all()
+        # Line and Column
+        for i in range(3):
+            line = self.board[i]
+            column = []
+            for j in range(3):
+                column.append(self.board[j][i])
+            if winner(line) or winner(column):
+                print("Winner")
+                return True
+        # Diagonals
+        d_1 = [self.board[0][0],self.board[1][1],self.board[2][2]]
+        d_2 = [self.board[0][2],self.board[1][1],self.board[2][0]]
+        
+        return winner(d_1) or winner(d_2)
+    
+    def play(self):
+        self.reset_board()
+        while self.available:
+            while self.player:
+                print("Moves available: ", self.available)
+                p = input('Give a valid cordinate, ex: "1, 1" (if available)')
+                self.player = self.check_valid_player_input(p)
+                if self.check_winner():
+                    return "player O wins"
+                elif not self.available:
+                    return "Draw"
+            m = random.choices(self.available)[0] 
+            self.available.remove(m)
+            self.board[m[0]][m[1]] = self.player
+            self.player = not self.player
+            self.print_board()
+            if self.check_winner():
+                return "player X wins"
+            
+        return "Draw"
+    
+    def print_board(self):
+        for line in self.board:
+            line_str = str(line)
+            line_str = line_str.replace('False', 'X')
+            line_str = line_str.replace('True', 'O')
+            line_str = line_str.replace('None', '_')
+            print(line_str)
+        return None
+    
+    def reset_board(self):
+        self.board = [[None,None,None],
+                     [None,None,None],
+                     [None,None,None]]
+        self.available = [(0,0),(0,1),(0,2),
+                         (1,0),(1,1),(1,2),
+                         (2,0),(2,1),(2,2)]
+        
 
 def rotate_board(board):
     sequece = [(0,0),(0,1),(0,2),
@@ -68,23 +119,18 @@ def flip_vertical(board):
               [board[2][2],board[2][1],board[2][0]]]    
     return _board
 
-if __name__=="__main__":
-    all_boards = []
-    flip_switch = {1:flip_horizontal,
-                   0:flip_vertical}
-    
-    board_init = [['1','2','3'],
-                 ['4','5','6'],
-                 ['7','8','9']]
-    all_boards.append(board_init)
-    board = board_init
-    for n_rotations in range(8):
-        board = rotate_board(board)
-        if not board in all_boards:
-            all_boards.append(board)
-    
-        for n_flip in range(4):
-            board = flip_switch[n_flip%2](board)
-            if not board in all_boards:
-                all_boards.append(board)
 
+def main():
+    ttt = TicTacToe()
+    p_input  = 'yes'
+    while p_input == 'yes':
+        result = ttt.play()
+        print(result)
+        print("-"*20)
+        p_input = input("Play again? (yes, no)").lower()
+        print("-"*20)
+        
+    
+if __name__=="__main__":
+    main()
+    
